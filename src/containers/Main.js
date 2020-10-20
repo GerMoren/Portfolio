6110646537ecfbfee7cc2b6cba22f404c9e0f1dd
GerMoren <ingenieriamg91@gmail.com>
2020-10-20 00:56:14 -0300

    vercel

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,7 +8,7 @@ import Projects from "./projects/Projects";
 // import StartupProject from "./StartupProjects/StartupProject";
 // import Achievement from "./achievement/Achievement";
 // import Blogs from "./blogs/Blogs";
-import Contact from "./contact/Contact";
+// import Contact from "./contact/Contact";
 import Footer from "../components/footer/Footer";
 // import Talks from "./talks/Talks";
 // import Podcast from "./podcast/Podcast";
@@ -17,7 +17,7 @@ import Top from "./topbutton/Top";
 // import Twitter from "./twitter-embed/twitter";
 import { StyleProvider } from "../contexts/StyleContext";
 import "./Main.css";
-// import Profile from "./profile/Profile";
+import Profile from "./profile/Profile";
 // import { educationInfo } from "../portfolio";
 
 export default class Main extends Component {
@@ -48,15 +48,15 @@ export default class Main extends Component {
           {/* <StackProgress /> */}
           <Education />
           {/* <WorkExperience /> */}
-          <Projects />
           {/* <StartupProject /> */}
           {/* <Achievement /> */}
           {/* <Blogs /> */}
           {/* <Talks /> */}
           {/* <Twitter /> */}
           {/* <Podcast /> */}
-          {/* <Profile /> */}
-          <Contact />
+          <Projects />
+          <Profile />
+          {/* <Contact /> */}
           <Footer />
           <Top />
         </StyleProvider>
