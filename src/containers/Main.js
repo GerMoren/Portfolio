8d5d3ce6adabc905e948b6d799301d0c55b6af28
GerMoren <ingenieriamg91@gmail.com>
2020-10-20 01:09:41 -0300

    git

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,7 +4,7 @@ import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
 // import StackProgress from "./skillProgress/skillProgress";
 // import WorkExperience from "./workExperience/WorkExperience";
-import Projects from "./projects/Projects";
+// import Projects from "./projects/Projects";
 // import StartupProject from "./StartupProjects/StartupProject";
 // import Achievement from "./achievement/Achievement";
 // import Blogs from "./blogs/Blogs";
@@ -17,7 +17,7 @@ import Top from "./topbutton/Top";
 // import Twitter from "./twitter-embed/twitter";
 import { StyleProvider } from "../contexts/StyleContext";
 import "./Main.css";
-// import Profile from "./profile/Profile";
+import Profile from "./profile/Profile";
 // import { educationInfo } from "../portfolio";
 
 export default class Main extends Component {
@@ -54,8 +54,8 @@ export default class Main extends Component {
           {/* <Talks /> */}
           {/* <Twitter /> */}
           {/* <Podcast /> */}
-          <Projects />
-          {/* <Profile /> */}
+          {/* <Projects /> */}
+          <Profile />
           <Contact />
           <Footer />
           <Top />
